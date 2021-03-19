026385e1d8ddcd2011d3a4352e128ec6e99fb06c
bamamoudou <mamoudouba8787@gmail.com>
2021-03-19 11:20:23 +0100

    Init project

---
+++ mediscreen-patient/src/test/java/com/mediscreen/mediscreenpatient/MediscreenPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
