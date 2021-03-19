437cc99e82ad91a726363efe162f31040b5d80a3
bamamoudou <mamoudouba8787@gmail.com>
2021-03-19 12:17:49 +0100

    Adding edge microservice project

---
+++ edge-microservices/mediscreen-admin/src/test/java/com/mediscreen/mediscreenadmin/MediscreenAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
