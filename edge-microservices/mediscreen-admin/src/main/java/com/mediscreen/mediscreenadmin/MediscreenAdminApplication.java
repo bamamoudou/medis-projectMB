53e26b12595a10cc77138df0a29d1edb026709d1
bamamoudou <mamoudouba8787@gmail.com>
2021-03-19 12:54:42 +0100

    Adding edge microservice project

--- edge-microservices/mediscreen-admin/src/main/java/com/mediscreen/mediscreenadmin/MediscreenAdminApplication.java
+++ edge-microservices/mediscreen-admin/src/main/java/com/mediscreen/mediscreenadmin/MediscreenAdminApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
 public class MediscreenAdminApplication {
 
 	public static void main(String[] args) {
