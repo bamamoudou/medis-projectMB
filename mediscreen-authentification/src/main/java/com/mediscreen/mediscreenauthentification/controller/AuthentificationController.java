51194a224003d24fe19ae583498d3e2eb0fe5df2
bamamoudou <mamoudouba8787@gmail.com>
2021-03-22 17:48:03 +0100

    Change eureka client port

---
+++ mediscreen-authentification/src/main/java/com/mediscreen/mediscreenauthentification/controller/AuthentificationController.java
@@ -0,0 +1,34 @@
+package com.mediscreen.mediscreenauthentification.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mediscreen.mediscreenauthentification.model.Jwt;
+import com.mediscreen.mediscreenauthentification.model.Login;
+import com.mediscreen.mediscreenauthentification.service.SecurityService;
+
+@RestController
+public class AuthentificationController {
+	@Autowired
+	private SecurityService securityService;
+
+	@PostMapping("/generate-token")
+	public ResponseEntity<Jwt> generateToken(@Valid @RequestBody Login userLogin) {
+		return new ResponseEntity<>(securityService.logUser(userLogin), HttpStatus.OK);
+	}
+
+	@GetMapping("/validate-token")
+	public ResponseEntity<Void> validateToken(@RequestParam String token) {
+		if (securityService.isLog(token))
+			return ResponseEntity.status(HttpStatus.OK).build();
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+	}
+}
