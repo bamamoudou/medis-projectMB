a98e2827e1fab738f6b59a889e7a79ed94c547e0
bamamoudou <mamoudouba8787@gmail.com>
2021-03-22 16:06:10 +0100

    Create authentification service

---
+++ mediscreen-authentification/src/main/java/com/mediscreen/mediscreenauthentification/MediscreenAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenauthentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenAuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenAuthentificationApplication.class, args);
+	}
+
+}
