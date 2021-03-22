044e3ca5fa4947831271557cbd8cb80b10293356
bamamoudou <mamoudouba8787@gmail.com>
2021-03-22 15:20:28 +0100

    Create clientui project

---
+++ mediscreen-clientui/src/main/java/com/mediscreen/mediscreenclientui/MediscreenClientuiApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenclientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenClientuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenClientuiApplication.class, args);
+	}
+
+}
---
+++ mediscreen-clientui/src/test/java/com/mediscreen/mediscreenclientui/MediscreenClientuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenclientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenClientuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
