daf4f91901502d221f96cd49cc0503d0bcf7dc24
bamamoudou <mamoudouba8787@gmail.com>
2021-03-23 16:13:02 +0100

    Implement model layer for client ui

--- mediscreen-clientui/src/main/java/com/mediscreen/mediscreenclientui/utils/FileUtils.java
+++ mediscreen-clientui/src/main/java/com/mediscreen/mediscreenclientui/utils/FileUtils.java
@@ -1,5 +1,32 @@
 package com.mediscreen.mediscreenclientui.utils;
 
+import java.io.File;
+
+import org.apache.commons.lang.StringUtils;
+
 public class FileUtils {
 
+	public FileUtils() {
+	}
+
+	/**
+	 * Check is file exist
+	 * 
+	 * @param relativePath
+	 * @return
+	 */
+	public static String getFileExtention(String relativePath, String fileType) {
+		String extention = null;
+		if (!StringUtils.isBlank(relativePath)) {
+			if (fileType.equals("img")) {
+				if (new File(relativePath + ".png").exists())
+					extention = ".png";
+				if (new File(relativePath + ".jpg").exists())
+					extention = ".jpg";
+			} else if (new File(relativePath).exists()) {
+				extention = "unknown";
+			}
+		}
+		return extention;
+	}
 }
