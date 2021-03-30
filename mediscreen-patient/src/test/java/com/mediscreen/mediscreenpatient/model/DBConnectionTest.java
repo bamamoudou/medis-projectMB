54f0d9df56f004309948a8c97ae6dbdda247e294
bamamoudou <mamoudouba8787@gmail.com>
2021-03-30 17:40:58 +0200

    Adding model unit test

---
+++ mediscreen-patient/src/test/java/com/mediscreen/mediscreenpatient/model/DBConnectionTest.java
@@ -0,0 +1,54 @@
+package com.mediscreen.mediscreenpatient.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DBConnectionTest {
+    private DBConnection dbConnection;
+
+    @BeforeEach
+    void init_test(){
+        dbConnection = new DBConnection(
+                "host",
+                1111,
+                "db",
+                "user",
+                "password"
+        );
+    }
+
+    @Tag("DBConnectionTest")
+    @Test
+    void get_test(){
+        assertThat(dbConnection.getHost()).isEqualTo("host");
+        assertThat(dbConnection.getPort()).isEqualTo(1111);
+        assertThat(dbConnection.getDatabase()).isEqualTo("db");
+        assertThat(dbConnection.getUser()).isEqualTo("user");
+        assertThat(dbConnection.getPassword()).isEqualTo("password");
+    }
+
+    @Tag("DBConnectionTest")
+    @Test
+    void set_test(){
+        assertThat(dbConnection.getHost()).isEqualTo("host");
+        assertThat(dbConnection.getPort()).isEqualTo(1111);
+        assertThat(dbConnection.getDatabase()).isEqualTo("db");
+        assertThat(dbConnection.getUser()).isEqualTo("user");
+        assertThat(dbConnection.getPassword()).isEqualTo("password");
+
+        dbConnection.setHost("new_host");
+        dbConnection.setPort(2222);
+        dbConnection.setDatabase("new_db");
+        dbConnection.setUser("new_user");
+        dbConnection.setPassword("new_password");
+
+        assertThat(dbConnection.getHost()).isEqualTo("new_host");
+        assertThat(dbConnection.getPort()).isEqualTo(2222);
+        assertThat(dbConnection.getDatabase()).isEqualTo("new_db");
+        assertThat(dbConnection.getUser()).isEqualTo("new_user");
+        assertThat(dbConnection.getPassword()).isEqualTo("new_password");
+    }
+}
