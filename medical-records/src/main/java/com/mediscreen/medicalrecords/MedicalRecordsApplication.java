70b60368036f93908fbef799ec6cc7d8852c5fed
bamamoudou <mamoudouba8787@gmail.com>
2021-04-01 14:49:23 +0200

    Configure feign server and adding swagger

--- medical-records/src/main/java/com/mediscreen/medicalrecords/MedicalRecordsApplication.java
+++ medical-records/src/main/java/com/mediscreen/medicalrecords/MedicalRecordsApplication.java
@@ -2,8 +2,15 @@ package com.mediscreen.medicalrecords;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
+@EnableFeignClients("com.mediscreen.medicalrecords")
 public class MedicalRecordsApplication {
 
 	public static void main(String[] args) {
