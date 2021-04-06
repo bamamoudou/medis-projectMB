63a193a9f889cac15be7d3b12074d55c4765ca3f
bamamoudou <mamoudouba8787@gmail.com>
2021-04-06 20:23:25 +0200

    Project init

---
+++ medical-report/src/main/java/com/mediscreen/medicalreport/MedicalReportApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.medicalreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalReportApplication.class, args);
+	}
+
+}
---
+++ medical-report/src/test/java/com/mediscreen/medicalreport/MedicalReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.medicalreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
