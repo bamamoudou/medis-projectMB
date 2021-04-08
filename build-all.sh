7ea91e7a6975763b415e52603a2cf5367ee0c0a3
bamamoudou <mamoudouba8787@gmail.com>
2021-04-08 19:19:56 +0200

    Adding Docker compose

---
+++ build-all.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd ./edge-microservices/ems-eureka
+./build.sh
+
+cd ../../edge-microservices/ems-zuul
+./build.sh
+
+cd ../../edge-microservices/ems-admin
+./build.sh
+
+cd ../../ms-authentication
+./build.sh
+
+cd ../ms-patientAdmin
+./build.sh
+
+cd ../ms-medicalrecord
+./build.sh
+
+cd ../ms-clientui
+./build.sh
