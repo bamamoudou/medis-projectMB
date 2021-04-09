bea562ebfc0d66bfa13b1de25995f2f514f799e4
bamamoudou <mamoudouba8787@gmail.com>
2021-04-09 15:31:02 +0200

    Project init

--- medical-records/src/test/java/com/mediscreen/medicalrecords/JWTtest.java
+++
@@ -1,6 +0,0 @@
-package com.mediscreen.medicalrecords;
-
-public class JWTtest {
-	public static String token = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTgwODA1NTgsInVzZXJJRCI6IjA5MzE4NDlhLThkNjgtNGQ2Yy1hOTNlLTg0ODc3ZDk1MzE5MSIsInVzZXJuYW1lIjoicGFzc3dvcmQifQ.CZPmiEOPHVujJsIO8e-OMa6jRq_BLUgjIBRgkNRrnCk";
-	public static String wrongToken = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTgwODA1NTgsInVzZXJJRCI6IjA5MzE4NDlhLThkNjgtNGQ2Yy1hOTNl";
-}
