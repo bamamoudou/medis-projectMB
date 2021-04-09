87aa43e4c1cc1603d51d31b74bf0458b92c06d2d
bamamoudou <mamoudouba8787@gmail.com>
2021-04-09 12:36:12 +0200

    Refactoring code

--- mediscreen-clientui/src/main/resources/static/scripts/patient-sheet_script.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-    $('.btn_tab').on('click', function(){
-        $($(this).attr('data-btn-target-class')).addClass('none').removeClass('block');
-        $($(this).attr('data-btn-target-id')).addClass('block').removeClass('none');
-        $('.btn_tab').removeClass('active');
-        $(this).addClass('active');
-    });
-})
